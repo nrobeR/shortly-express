6800838d77078658ceb9453aca585717634697b2
nrobeR <mr.nrober@gmail.com>
2014-08-18 17:34:39 -0700

    At logging page

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,17 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var crypto = require('crypto');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function(){
+    this.on('creating',function(model,attrs,options){
+      var shasum = crypto.createHash('sha1');
+      shasum.update(model.get('password'));
+      model.set('password',shasum.digest('hex'));
+    });
+  }
 });
 
 module.exports = User;
--- shortly.js
+++ shortly.js
@@ -23,24 +23,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links',
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links',
 function(req, res) {
   var uri = req.body.url;
 
@@ -74,9 +74,55 @@ function(req, res) {
   });
 });
 
+
 /************************************************************/
 // Write your authentication routes here
 /************************************************************/
+
+app.get('/login',
+function(req, res) {
+  res.render('login');
+});
+
+app.get('/signup',
+function(req, res) {
+  res.render('signup');
+});
+
+app.post('/signup',
+function(req,res){
+  // console.log('signup req.body:', req.body);
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log('username', username, 'password', password);
+
+  new User({ username: username }).fetch().then(function(found) {
+    if(found) {
+      res.send(200, 'Username ' + username + ' exists!');
+    } else {
+      var user = new User({
+        username: username,
+        password: password
+      });
+
+      user.save().then(function(newUser) {
+        Users.add(newUser);
+        res.send(200, 'Your account, ' + username + ', has been created.');
+      });
+    }
+  });
+
+});
+
+app.post('/login',
+function(req,res){
+  var username = req.body.username;
+  var password = req.body.password;
+  console.log('username', username, 'password', password);
+  Users.reset().fetch().then(function(users) {
+    console.log(users.models[0]);
+  });
+});
 
 
 
